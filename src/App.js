b1cf37eac4fab2fef3a3f470fc8cae63a41b075f
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-09-18 09:12:37 +0530

    about page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import Header from "./Components/Header"; // Header is a default export
 import { Footer } from "./Components/Footer"; // Footer is a named export, {} required to import arrow functions
 import { Home } from "./Components/Home/Home";
-
+import { About } from "./Components/About/About";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Header title="Todos List" searchBar={false}></Header>
-      <Home></Home>
-      <Footer></Footer>
-    </div>
+    <BrowserRouter>
+      <Header title="Todos List" searchBar={false} />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 export default App;
