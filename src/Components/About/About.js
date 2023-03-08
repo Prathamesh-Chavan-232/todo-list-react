f273ba0fec292376ddb92d0f7bfd474ddd3c1637
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-03-08 20:58:11 +0530

    refactor: build for production

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,21 +2,21 @@ import "../../App.css";
 import React from "react";
 
 export const About = () => {
-	return (
-		<div>
-			<h2>This is the About page</h2>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab modi nulla
-				quaerat beatae culpa fugiat, a quas voluptates consequatur, ipsam quidem
-				commodi eum cumque ad animi quam corrupti! Quibusdam quisquam ex ratione
-				unde ab, autem ipsa at ipsum quidem praesentium, sint rem eveniet nisi
-				ea quos tenetur! Unde dolores assumenda officiis, aut architecto cum
-				modi deserunt fugit labore quia quod ut excepturi eius commodi error?
-				Consectetur pariatur minus aliquam distinctio quod reiciendis ipsam
-				quasi iste voluptates sit dolorem, libero quo placeat corrupti ratione
-				unde inventore enim aliquid molestias nulla repellat at eveniet
-				provident! Corporis rerum vero ab nam quo harum?
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <h2>This is the About page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab modi nulla
+        quaerat beatae culpa fugiat, a quas voluptates consequatur, ipsam quidem
+        commodi eum cumque ad animi quam corrupti! Quibusdam quisquam ex ratione
+        unde ab, autem ipsa at ipsum quidem praesentium, sint rem eveniet nisi
+        ea quos tenetur! Unde dolores assumenda officiis, aut architecto cum
+        modi deserunt fugit labore quia quod ut excepturi eius commodi error?
+        Consectetur pariatur minus aliquam distinctio quod reiciendis ipsam
+        quasi iste voluptates sit dolorem, libero quo placeat corrupti ratione
+        unde inventore enim aliquid molestias nulla repellat at eveniet
+        provident! Corporis rerum vero ab nam quo harum?
+      </p>
+    </div>
+  );
 };
